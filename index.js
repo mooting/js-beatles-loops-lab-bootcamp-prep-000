569cacf072805c8e6f86ca5de8e09ededd0b44b5
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 21:48:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function johnLennonFacts( facts ) {
   let i = 0,
     array = []
   while ( i < facts.length ) {
-    array.push( `${ facts[i] }!` )
+    array.push( `${ facts[i] }!!!` )
     i++
   }
   return array
