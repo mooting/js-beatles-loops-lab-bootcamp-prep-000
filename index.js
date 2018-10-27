063f57e4dba35209354d5c842d265651806d9de0
Justin Davis <justin_w_davis@yahoo.com>
2018-10-27 21:41:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay( musicians, instruments ) {
   let array = []
   for ( let i = 0; i < musicians.length; i++ )
-    array.push( `${ musicians[i] } plays ${ instrument[i] }`)
+    array.push( `${ musicians[i] } plays ${ instruments[i] }`)
 
-  return array  
+  return array
 }
